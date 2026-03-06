283b46dbb867069b0a7e9fe6ffd9def2da67243a
Ilonczai András <ilonczaia@griffsoft.hu>
2026-03-06 10:50:04 +0100

    init project

---
+++ src/main/java/com/kronohealth/KronoHealthApplication.java
@@ -0,0 +1,12 @@
+package com.kronohealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KronoHealthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KronoHealthApplication.class, args);
+    }
+}
